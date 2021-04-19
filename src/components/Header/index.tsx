d83f072b95e23f22a77ceaae60ad35cc2f062b47
Leonardo Sanches <sleonardogabriel@gmail.com>
2021-04-19 14:51:31 -0300

    Recuperação das informações do prismic e funcionalidade do nav bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
+
 import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Link href="/">
+          <img src="/images/logo.svg" alt="ig.news" />
+        </Link>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
