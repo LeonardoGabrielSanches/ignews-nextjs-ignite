48c96e9f3ec100aaad6634e2f97328630cd5e3b4
Leonardo Sanches <sleonardogabriel@gmail.com>
2021-04-19 15:37:30 -0300

    Pagina de post com problemas de tipagem no código

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
 
@@ -27,11 +29,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
@@ -48,7 +52,7 @@ export const getStaticProps: GetStaticProps = async () => {
     fetch: ['post.title', 'post.content'],
     pageSize: 100
   });
- 
+
   const posts = response.results.map(post => {
     return {
       slug: post.uid,
@@ -61,7 +65,7 @@ export const getStaticProps: GetStaticProps = async () => {
       })
     };
   });
-  
+
   return {
     props: {
       posts
